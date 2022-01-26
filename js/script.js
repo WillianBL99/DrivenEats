a7745889a035d1a150358f9580dc98dc877382da
Paulo Uilian <uilianlbarros@gmail.com>
2022-01-25 22:15:32 -0300

    Corrigindo problemas com data-indentifier

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function select_food() {
     let food = document.getElementById("opc_food_1");
-    food.style.border = "3px solid green";
+    food.style.border = "3px solid #32B72F";
     food.style.padding = "11px";
 }
