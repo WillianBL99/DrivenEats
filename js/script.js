c9e54609a40ba80a74fcabe1fe3bada658468940
Paulo Uilian <uilianlbarros@gmail.com>
2022-01-29 21:07:01 -0300

    Subistitui forma de inserir a borda nos itens selecionados

--- js/script.js
+++ js/script.js
@@ -25,9 +25,13 @@ let valTot
 /* Fução que marca os items selecionados */
 function selectFood(element, classFather){
     const selectedFood = document.querySelector(`#${classFather} .selected`);
-    if(selectedFood !== null && selectedFood !== element) selectedFood.classList.remove('selected');
-
+    if(selectedFood !== null && selectedFood !== element) {
+        selectedFood.classList.remove('selected');
+        selectedFood.querySelector('button').classList.add('hide');
+    }
+    
     element.classList.toggle('selected');
+    element.querySelector('button').classList.toggle('hide');
     btn_active();
 }
 
